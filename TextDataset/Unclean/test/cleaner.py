2c134c29fa12562a6e6d5ba37bcf1930e449c345
S Shivansh <shivansh.s@research.iiit.ac.in>
2020-06-05 21:04:19 +0530

    Adding Hindi sentences

--- TextDataset/Unclean/test/cleaner.py
+++ TextDataset/Unclean/test/cleaner.py
@@ -9,19 +9,19 @@ c = 0
 
 tweets_raw = []
 f = open('cleaned_data.txt', 'w+')
+h = open('hindi.txt', 'w+')
 
 with open('data.txt','r') as file:      
     for line in file:   
         tweets_raw.append(line)
 
 tweets_english = []
-tweets_hindi = []
 c = 0
 
 for i in tweets_raw:
 
     i = str(i)
-    i = re.sub('(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*(\?[a-z\_\=\&\;\-]*))',' ', i)
+    i = re.sub('(https?:\/\/)?([\da-z\.-]+)\.(([a-z\.]{2,6})([\/\w \.-]*(\?[a-z\_\=\&\;\-]*))*)',' ', i)
     i = re.sub('(@[A-Za-z0-9]+)', ' ', i)
     i = re.sub('(&[A-Za-z]*\;)', ' ', i)
     try:
@@ -29,7 +29,7 @@ for i in tweets_raw:
         if lang == "en" :
             tweets_english.append(i)
         elif lang == "hi" :
-            tweets_hindi.append(i)
+            h.write(i)
     except:
         pass
 
