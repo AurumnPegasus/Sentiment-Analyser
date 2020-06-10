7f0b48c74b0603f1e18e12d1b67a8ed653b051f5
S Shivansh <shivansh.s@research.iiit.ac.in>
2020-06-10 16:13:27 +0530

    Updated Dataset

--- Extract/cleaner.py
+++ Extract/cleaner.py
@@ -50,6 +50,17 @@ def create_lemmatized_sent(words):
             cleaned.append(element)
     return cleaned
 
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
 def write_sent(sent):
     cleaned = []
     for i in sent:
@@ -58,6 +69,7 @@ def write_sent(sent):
             j = str(j)
             j = j + " "
             s = s + j
+        s = remove_emoji(s)
         element = [s, i[1]]
         cleaned.append(element)
     df = pd.DataFrame(cleaned)
